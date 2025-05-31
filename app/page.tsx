2bae1f14109a17c9b8fb89858fc05cdcb36f55b7
dpa313 <dpa.github@gmail.com>
2025-05-31 23:23:10 +0600

    theme added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button>click me</Button>
+    <div className="">
+      
     </div>
   );
 }
